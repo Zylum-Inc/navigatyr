a4dd07312baa73752d322d13f4aff24b11f5e641
dehiryder <sidd.gupta@gmail.com>
2023-06-29 19:01:26 -0400

    Checkpoint: chugging along the rust-cli tutorial. Still not doing *anything* useful.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+#![allow(unused)]
+
+use std::io::{BufRead, BufReader};
+use clap::Parser;
+
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    println!("pattern is {pattern} and path is {path:?}",
+             pattern = args.pattern,
+             path = args.path);
+
+    let mut reader = BufReader::new(std::fs::File::open(&args.path).expect("could not open file"));
+    let mut line = String::new();
+
+    loop {
+        let len = reader.read_line(&mut line).expect("could not read line");
+        if len == 0 {
+            break;
+        }
+        if line.contains(&args.pattern) {
+            print!("{}", line);
+        }
+        line.truncate(0);
+    }
 }
