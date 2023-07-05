ee4c54f860dc142a213a1751e03089bf91061a7e
dehiryder <sidd.gupta@gmail.com>
2023-07-05 18:24:50 -0400

    Checkpoint: Started added a structure for the command line tool. (making it more tyr like). Code still panics on the arduino-cli.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,67 @@
 #![allow(unused)]
 
 use std::io::{BufRead, BufReader};
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use anyhow::{Context, Result};
+use arduino_cli_client::commands::arduino_core_client::ArduinoCoreClient;
+use arduino_cli_client::commands::{BoardListReq, InitReq};
 
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct Cli {
-    /// The pattern to look for
-    pattern: String,
-    /// The path to the file to read
-    path: std::path::PathBuf,
+    #[command(subcommand)]
+    command: TyrCommands,
 }
 
-fn main() -> Result<()> {
+#[derive(Subcommand)]
+enum TyrCommands {
+    /// Sub commands related to the boostrap phase of the firmware lifecycle
+    Bootstrap {
+        family: String,
+        #[command(subcommand)]
+        command: TyrBootstrapCommands,
+    }
+}
+
+#[derive(Subcommand, Debug)]
+enum TyrBootstrapCommands {
+    /// Show connected devices
+    ListDevices,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
 
-    println!("pattern is {pattern} and path is {path:?}",
-             pattern = args.pattern,
-             path = args.path);
+    match args.command {
+        TyrCommands::Bootstrap { family, command } => {
+            println!("Bootstrapping {} subcommand {:?}", family, command);
 
-    let path = &args.path;
+            let mut client = ArduinoCoreClient::connect("http://localhost:50051").await?;
 
-    let content = std::fs::read_to_string(path)
-        .with_context(|| format!("could not read file `{}`", path.display()))?;
+            // Start a new instance of the Arduino Core Service
+            let mut init_stream = client
+                .init(InitReq {
+                    library_manager_only: false,
+                })
+                .await?
+                .into_inner();
 
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+            let resp_instance = init_stream.message().await?.expect("Failed to init");
+
+            // List the boards currently connected to the computer.
+            let resp_boards = client
+                .board_list(BoardListReq {
+                    instance: resp_instance.instance,
+                })
+                .await?
+                .into_inner();
+
+            print!("Boards: {:?}", resp_boards.ports);
+
         }
     }
+
     Ok(())
 }
