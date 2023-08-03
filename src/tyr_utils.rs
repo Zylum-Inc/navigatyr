9f61471c1ee4b304163c4f217498a20c63304eba
sidd.gupta <sidd.gupta@bluedolph.in>
2023-08-03 14:46:18 -0400

    Added printing of stdout and stderr in case of process_command failures.

--- src/tyr_utils.rs
+++ src/tyr_utils.rs
@@ -20,6 +20,8 @@ pub fn process_command(command: &[&str], error_msg: &str) -> Result<(), Error> {
 
     if !output.status.success() {
         println!("{}", error_msg);
+        println!("Command output: {}", String::from_utf8_lossy(&output.stdout));
+        println!("Command error: {}", String::from_utf8_lossy(&output.stderr));
         std::process::exit(1);
     } else {
         println!("Command output: {}", String::from_utf8_lossy(&output.stdout));
